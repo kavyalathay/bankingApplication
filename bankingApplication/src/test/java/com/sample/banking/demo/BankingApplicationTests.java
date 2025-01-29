d04952daa4b1ca6d7df2ab3c51670cd0077a0ea4
kavyerram <kavyerram@USPARKAVYERRAM4>
2025-01-29 14:48:39 -0600

    initial commit

---
+++ bankingApplication/src/test/java/com/sample/banking/demo/BankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.banking.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
